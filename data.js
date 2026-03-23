787f308352fd14eff0ee0df791966590bb3268cc
dogierez01 <kalimera54@gmail.com>
2026-03-23 22:34:45 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+// This file holds ALL your lesson content. 
+const courseData = [
+    {
+        category: "Human Resources (HR)",
+        situations: [
+            { 
+                id: 1, 
+                title: "Asking for a Raise", 
+                audioFile: "01_Human_Resources_HR_Asking_for_a_Raise.m4a",
+                dialogue: `
+                    <p class="line"><span class="speaker-A">Alex:</span> Good morning, Mr. Davis. Thank you for taking the time to <span class="vocab" data-tr="görüşmek">discuss</span> my role today.</p>
+                    <p class="line"><span class="speaker-B">Mr. Davis:</span> Good morning, Alex. Have a seat. What's on your mind?</p>
+                    <p class="line"><span class="speaker-A">Alex:</span> I believe my <span class="vocab" data-tr="katkı">contributions</span> have increased. I would like to request a <span class="vocab" data-tr="maaş zammı">raise</span>.</p>
+                `
+            },
+            { 
+                id: 2, 
+                title: "The First Day", 
+                audioFile: "02_Human_Resources_HR_The_First_Day.m4a",
+                dialogue: `
+                    <p class="line"><span class="speaker-A">HR:</span> Welcome to the company! We are <span class="vocab" data-tr="heyecanlı">excited</span> to have you.</p>
+                `
+            }
+            // You will just keep adding the rest of the situations here!
+        ]
+    }
+];
